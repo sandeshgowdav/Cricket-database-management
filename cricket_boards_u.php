1fed2e01ca324c50d1788be03601030efd527085
sandeshgowdav <119774281+sandeshgowdav@users.noreply.github.com>
2024-02-15 11:11:03 +0530

    Add files via upload

---
+++ cricket_boards_u.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<style >
+	body {font-family: Arial, Helvetica, sans-serif; background-repeat: no repeat; color: black; text-transform: uppercase;
+  background-size: 100%;
+}
+	table{
+		border: 2px solid black; height:100px; width:900px; font-size: 30px;
+	}
+	tr{
+		border: 2px solid black; height:100px; width:800px;
+	}
+	th{
+		border: 2px solid black ; height:100px; width:800px; 
+	}
+	.fade-in-text {
+  font-family: Arial;
+  font-size: 60px;
+  animation: fadeIn 5s;
+  -webkit-animation: fadeIn 5s;
+  -moz-animation: fadeIn 5s;
+  -o-animation: fadeIn 5s;
+  -ms-animation: fadeIn 5s;
+}
+
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@-moz-keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@-webkit-keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@-o-keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@-ms-keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+</style>
+<head>
+	<title>CRICKET BOARDS</title>
+</head>
+
+
+<body class="fade-in-text" background="img/bg1.jpg"> 
+<div ><table align="center" width="100%"><tr><th><p><h1>Cricket Boards</h1></p></th></tr></div>
+	<table align="center">
+		<tr>
+			<th>Board Name</th>
+			<th>Chairman</th>
+			<th>Team's Board</th>
+			<th>DOA</th>
+		</tr>
+		<?php
+		$con=mysqli_connect("localhost","root","","cricket");
+		$query="select * from cricket_boards";
+		$result=mysqli_query($con,$query);
+		if(mysqli_num_rows($result)>0)
+		{
+			while ($row=mysqli_fetch_assoc($result)) {
+			echo "<tr><th>".$row["board_name"]."</th>"."<th>".
+			$row["chairman"]."</th><th>".
+			$row["team"]."</th><th>".
+			$row["DOA"]."</th></tr>";
+			}
+		}
+		mysqli_close($con);
+		?>
+	</table>
+</body>
+</html>
---
+++ schedule.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<style >body {background-repeat: no repeat; text-transform: uppercase; font-family: Arial, Helvetica, sans-serif;
+  background-size: 100%;
+}
+	table{
+		border: 2px solid black; font-size: 30px; height:100px; width:900px;
+	}
+	tr{
+		border: 1px solid black;
+	}
+	th{
+		border: 1px solid black;
+	}
+</style>
+<head><p>
+	<title>SCHEDULES</title>
+</head>
+<body background="img/bg1.jpg">
+<div ><table align="center" width="30%"><tr><th><p><h1 style="color: black;">SCHEDULES</h1></p></th></tr></div>
+	 <div style="margin-top:100px;">
+	<table align="center">
+		<tr>
+			<th>Date</th>
+			<th>Team1</th>
+			<th>Team2</th>
+			<th>Match Number</th>
+		</tr>
+		<?php
+		$con=mysqli_connect("localhost","root","","cricket");
+		$query="select *from schedules order by date";
+		$result=mysqli_query($con,$query);
+		if(mysqli_num_rows($result)>0)
+		{
+			while ($row=mysqli_fetch_assoc($result)) {
+			echo "<tr><th>".$row["date"]."</th><th>".
+			$row["team1"]."</th><th>".
+			$row["team2"]."</th><th>".
+			$row["match_no"]."</th><th>";
+			}
+		}
+		mysqli_close($con);
+		?>
+	</table>
+
+		 <form action="tt.php" method="post">
+		<tr align="center" width="300%" style="margin-top:200px;">
+		 <div style="margin-top:50px; font-size:30px;">
+            <p style="text-align: center;">Enter Match Number to retrieve players
+            <input type="number" style=font-size:25px; name="match_no"><br></br>
+            <input type="submit" style="float:center; font-size:30px; background:black; color:white;" value="Submit">
+            </p></tr>
+    </form>
+</body>
+</html>
